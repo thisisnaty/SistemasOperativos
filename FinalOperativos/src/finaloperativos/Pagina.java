a8d48da444bb33b942696da35e95c377ba1694dd
thisisnaty <garcia.doe@gmail.com>
2015-05-12 13:15:08 -0500

    clases de Pagina y Proceso

---
+++ FinalOperativos/src/finaloperativos/Pagina.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package finaloperativos;
+
+/**
+ *
+ * @author nataliagarcia
+ */
+public class Pagina {
+    private boolean bitRef;
+    private boolean bitMod;
+    private boolean bitResidencia;
+    
+    public Pagina() {
+        bitRef = false;
+        bitMod = false;
+        bitResidencia = false;
+    }
+}
---
+++ FinalOperativos/src/finaloperativos/Proceso.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package finaloperativos;
+
+/**
+ *
+ * @author nataliagarcia
+ */
+public class Proceso {
+    private int id;
+    private int numPaginas;
+    private int tamaño;
+    private Pagina[] arrPaginas;
+}
